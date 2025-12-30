349b34cd8dd0e5654d0f0cbdec7f8596ee92b522
Rana Prathap <rana@rana-pc.attlocal.net>
2025-12-30 13:39:46 -0800

    updates to host via github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hydroavia/',
+  // base: '/hydroavia/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
